638486dc5f265a4907a5a193ea2a7c9b44e8e943
Bainky <obainkyyyy@gmail.com>
2021-03-21 15:15:48 +0000

    🔨 Source code

--- middlewares/logging.py
+++ middlewares/logging.py
@@ -3,9 +3,6 @@
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.types import Message, CallbackQuery
 
-from logging import warning
-from loader import bot
-
 import json
 
 
@@ -16,15 +13,33 @@ class LoggingMiddleware(BaseMiddleware):
     otherwise how can you find out what caused the error?
     """
     async def on_process_message(self, message: Message, data: dict):
-        
-        parsed = json.loads(str(message))
-        res = json.dumps(parsed, indent=4, sort_keys=True, ensure_ascii=False)
-        
-        await bot.send_message(-1001229388428, res, parse_mode=None)
+
+        parsed = json.loads(
+            str(
+                message
+            )
+        )
+        res = json.dumps(
+            parsed,
+            indent=4,
+            sort_keys=True,
+            ensure_ascii=False
+        )
+
+        print(res)
 
     async def on_process_callback_query(self, call: CallbackQuery, data: dict):
 
-        parsed = json.loads(str(call))
-        res = json.dumps(parsed, indent=4, sort_keys=True, ensure_ascii=False)
-        
-        await bot.send_message(-1001229388428, res, parse_mode=None)
+        parsed = json.loads(
+            str(
+                call
+            )
+        )
+        res = json.dumps(
+            parsed,
+            indent=4,
+            sort_keys=True,
+            ensure_ascii=False
+        )
+
+        print(res)
